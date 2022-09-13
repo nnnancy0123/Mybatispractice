a426c414f4d80ac0d6f5566c90225c2ec6534651
Jin <106419088+nnnancy0123@users.noreply.github.com>
2022-09-13 19:16:31 +0900

    １
    １

---
+++ demo02/demo02/src/main/java/com/example/demo/bean/UserInfoBean.java
@@ -0,0 +1,14 @@
+package com.example.demo.bean;
+
+import lombok.Data;
+
+@Data
+public class UserInfoBean {
+	
+	 private String  name;
+	 
+	 private String  id;
+	 
+	 private String  age;
+	 
+}
